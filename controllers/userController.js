971eea75e756b78699be758ac4bc7d0559a0ecf3
satheeskaran <satheeskaran@huex.io>
2023-02-03 01:19:16 +0530

    profile image delete end point created

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const fs = require("fs");
 
 const getUser = asyncHandler(async (req, res) => {
   const { id } = req.params;
@@ -72,4 +73,43 @@ const changePassword = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "password changed successfully !" });
 });
 
-module.exports = { getUser, updateUser, changePassword };
+const removeProfilePicture = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  console.log("id is here", id);
+
+  if (!id) {
+    return res.status(404).json({ message: "user id not provided !" });
+  }
+
+  const user = await User.findById(id);
+
+  if (!user) {
+    return res.status(400).json({ message: "User does not exists !" });
+  }
+  if (user.profileImg) {
+    fs.unlink(`public/${user.profileImg}`, async (err) => {
+      if (err) {
+        return res.status(500).json({
+          message: "Could not delete the file. " + err,
+        });
+      }
+
+      user.profileImg = "";
+
+      const updatedUser = await user.save();
+
+      if (updateUser) {
+        res
+          .status(200)
+          .json({ message: "Profile picture deleted .", user: updatedUser });
+      } else {
+        return res.status(500).json({
+          message: "server error",
+        });
+      }
+    });
+  }
+});
+
+module.exports = { getUser, updateUser, changePassword, removeProfilePicture };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,10 +3,12 @@ const {
   getUser,
   updateUser,
   changePassword,
+  removeProfilePicture,
 } = require("../controllers/userController");
 
 router.get("/:id", getUser);
 router.patch("/:id", updateUser);
+router.delete("/remove-profile/:id", removeProfilePicture);
 router.patch("/change-password/:id", changePassword);
 
 module.exports = router;
