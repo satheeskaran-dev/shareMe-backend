8c496d2fbb9e2dcb04bd96f5e2ae1f415c258213
satheeskaran <satheeskaran@huex.io>
2023-03-01 20:48:39 +0530

    controller name changed

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const register = require("../controllers/authControllers/register");
 const login = require("../controllers/authControllers/login");
-const logOut = require("../controllers/authControllers/login");
+const logout = require("../controllers/authControllers/logout");
 
 router.post("/sign-up", register);
 router.post("/login", login);
-router.post("/login", login);
+router.post("/logout", logout);
 
 module.exports = router;
